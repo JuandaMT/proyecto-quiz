d4cc4e6664107367c79179742a4c1353a7acf3a5
JuandaMT <juandamt@outlook.es>
2023-06-19 19:04:28 +0200

    base de datos transformada

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-/* AXIOS */
-axios
-  .get(
-    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-  )
-  .then((res) => {
-      const questions = res.data.results
-  })
-  .catch((err) => console.error(err));
-  console.log(questions)
-/* ESTRUCTURA BASE DE DATOS 
-res.data.results = sale la estructura de las preguntas
-res.data.results[i].question = pregunta
-res.data.results[i].correct_answer = respuesta correcta
-res.data.results[i].incorrect_answers = respuestas incorrectas*/
-
 /* ME TRAIGO EL HTML A JS */
 const home = document.getElementById("home");
 const quiz = document.getElementById("quiz");
@@ -24,6 +8,67 @@ const questionContainer = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
 
+/* VARIABLES */
+
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+let questions = [];
+let question = "";
+let currentQuestionIndex;
+
+/* AXIOS */
+// const questions=[
+//     {
+//         question:"preguntaa?",
+//         answers:[
+//             {
+//                 text:"ey",
+//                 correct:true
+//             },
+//             {
+//                 text:"ey2",
+//                 correct:false
+//             }
+//         ]
+//     }
+// ]
+const myFunct = async () => {
+  try {
+    const res = await axios.get(API_URL);
+    questions = res.data.results;
+    questionsFormated = questions.map((question) => {
+      const incorrectAnswers = question.incorrect_answers.map((text) => {
+        return {
+          // text: text == text,
+          text,
+          correct: false,
+        };
+      });
+      const correctAnswer = { text: question.correct_answer, correct: true };
+      return {
+        question: question.question,
+        answers: [...incorrectAnswers, correctAnswer].sort(function(){return Math.random()-0.5}),
+      };
+    });
+    console.log(questionsFormated);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+myFunct();
+/* FUNCTIONS */
+const startGame = () => {
+  startBtn.classList.add("hide");
+  currentQuestionIndex = 0;
+  questionContainer.classList.remove("hide");
+
+  /* ESTRUCTURA BASE DE DATOS 
+res.data.results = sale la estructura de las preguntas
+res.data.results[i].question = pregunta
+res.data.results[i].correct_answer = respuesta correcta
+res.data.results[i].incorrect_answers = respuestas incorrectas*/
+};
 /* AÑADO CLASS LIST */
 const removePages = () => {
   home.classList.add("hide");
@@ -42,15 +87,6 @@ const goHome = () => {
 const goStats = () => {
   removePages();
   stats.classList.remove("hide");
-};
-
-/* VARIABLES */
-let currentQuestionIndex;
-/* FUNCTIONES */
-const startGame = () => {
-  startBtn.classList.add("hide");
-  currentQuestionIndex = 0;
-  questionContainer.classList.remove("hide");
 };
 /*  PARA PODER HACER ESTE PASO DEBO DE SABER RECOGER LOS DATOS DE LA API  */
 /* const showQuestion = (question) => {
